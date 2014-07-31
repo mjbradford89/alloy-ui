8871361ffcf063044e43b94fb2339dc8c6fb32d9
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-07-31 15:46:17 -0700

    AUI-1379 - Source formatting

--- src/aui-node/js/aui-node-accessible.js
+++ src/aui-node/js/aui-node-accessible.js
@@ -1,55 +1,49 @@
 /**
- * A set of utility methods to the Node to allow 'hiding'
+ * A set of utility methods for Node to allow 'hiding'
  * while still allowing screen readers access.
  *
  * @module aui-node-accessible
  * @submodule aui-node-accessible
  */
 
-var getClassName = A.getClassName,
+var CSS_BOOTSTRAP_SR_ONLY = A.getClassName('sr-only');
 
-    CSS_BOOTSTRAP_SR_ONLY = getClassName('sr-only'),
+A.mix(A.Node.prototype, {
 
-    NODE_PROTO = A.Node.prototype;
+    /**
+     * Hides the node.
+     *
+     * @method hideAccessible
+     */
+    hideAccessible: function() {
+        this.addClass(CSS_BOOTSTRAP_SR_ONLY);
 
-A.mix(
-    NODE_PROTO, {
+        this.after(this.showAccessible, this, 'show');
+    },
 
-        /**
-         * Hides the node.
-         *
-         * @method hideAccessible
-         */
-        hideAccessible: function() {
-            this.addClass(CSS_BOOTSTRAP_SR_ONLY);
+    /**
+     * Shows the node. Fires after the node-base 'show' method.
+     *
+     * @method showAccessible
+     */
+    showAccessible: function() {
+        this.removeClass(CSS_BOOTSTRAP_SR_ONLY);
+    },
 
-            this.after(this.showAccessible, this, 'show');
-        },
+    /**
+     * Toggles the node visibility.
+     *
+     * @param force
+     * @method toggleAccessible
+     */
+    toggleAccessible: function(force) {
+        force = (force !== undefined) ? force :
+                this.hasClass(CSS_BOOTSTRAP_SR_ONLY);
 
-        /**
-         * Shows the node.  Fires after the node-base 'show' method.
-         *
-         * @method showAccessible
-         */
-        showAccessible: function() {
-            this.removeClass(CSS_BOOTSTRAP_SR_ONLY);
-        },
-
-        /**
-         * If the node is hidden then it will be shown, if it is visible then it will be hidden.
-         *
-         * @param force
-         * @method toggleAccessible
-         */
-        toggleAccessible: function(force) {
-            var hide = (force !== undefined) ? force :
-                    !(this.hasClass(CSS_BOOTSTRAP_SR_ONLY));
-
-            if (hide) {
-                this.hideAccessible();
-            } else {
-                this.showAccessible();
-            }
+        if (force) {
+            this.showAccessible();
+        } else {
+            this.hideAccessible();
         }
     }
-);
+});
