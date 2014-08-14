741899d892075011a6c52b9d5c1ee6f01f3db37b
drewbrokke <drew.brokke@liferay.com>
2014-08-14 12:31:33 -0700

    AUI-1597 - uses aui-accessible-dd-drag for drag functionality

--- src/aui-accessible-dd/js/aui-accessible-dd-delegate.js
+++ src/aui-accessible-dd/js/aui-accessible-dd-delegate.js
@@ -32,11 +32,8 @@ var Delegate = A.Component.create({
                 index = instance.get('tabIndex');
 
             instance._handles.push(A.on('focus', A.bind(instance._onMouseEnter, instance), container));
-
             instance._handles.push(A.on('focus', A.bind(instance._onFocusChange, instance), container));
-
             instance._handles.push(A.on('blur', A.bind(instance._onMouseLeave, instance), container));
-
             instance._handles.push(
                 instance.dd.on('setTarget', function(event) {
                     if (DDM.activeDrag) {
@@ -45,17 +42,9 @@ var Delegate = A.Component.create({
                 })
             );
 
-            instance._bindKeypressEvent();
+            instance.once('drag:keyDown', instance._onCtrlKeydown, instance);
 
-            instance._plugNodeFocusManager();
-        },
-
-        _afterDragEnd: function(event) {
-            var instance = this,
-                drag = event.target,
-                dragNode = drag.get('node');
-
-            dragNode.focus();
+            container.on('key', A.bind(instance._onCtrlKeyup, instance), 'up:17');
 
             instance._plugNodeFocusManager();
         },
@@ -68,62 +57,15 @@ var Delegate = A.Component.create({
         */
         _onFocusChange: function(event) {
             var instance = this;
-            var target = event.target;
-            var nodesClass = instance.get('nodes');
 
-            instance.activeNode = target;
-        },
+            instance.dd.detachAll('drag:keyDown');
+            instance.dd.detachAll('drag:start');
 
-        /**
-        * TODO. Wanna help? Please send a Pull Request.
-        *
-        * @method _bindKeypressEvent
-        * @protected
-        */
-        _bindKeypressEvent: function(node) {
-            var instance = this;
-            var container = A.one(instance.get(CONT));
+            node = event.target;
 
-            instance.once('drag:keyDown', A.bind(instance._defKeyDownFn, instance));
-            instance.once('drag:start', A.bind(instance._onDragStart, instance));
-            instance.onceAfter('drag:end', A.bind(instance._afterDragEnd, instance));
+            instance.dd.set('node', node);
 
-            container.once('key', A.bind(instance._onCtrlKeydown, instance), 'down:17');
-        },
-
-        /**
-        * TODO. Wanna help? Please send a Pull Request.
-        *
-        * @method _defKeyDownFn
-        * @protected
-        */
-        _defKeyDownFn: function(event) {
-            var instance = this,
-                doc = A.one(DOC),
-                container = instance.get(CONT),
-                ev = event.ev;
-
-            ev.button = 1;
-            ev.currentTarget = instance.activeNode;
-
-            instance._delMouseDown(ev);
-
-            doc.once('key', A.bind(instance._onCtrlKeyup, instance), 'up:17');
-            event.halt();
-        },
-
-        /**
-         * Gets a list of valid drop targets for the drag object.
-         *
-         * @method _getDropTargets
-         * @protected
-         * @return {A.NodeList}
-         */
-        _getDropTargets: function() {
-            var instance = this;
-            var container = A.one(instance.get(CONT));
-
-            return container.all('.' + DDM.CSS_PREFIX + '-drop');
+            instance.dd._bindKeyEvents();
         },
 
         /**
@@ -136,9 +78,6 @@ var Delegate = A.Component.create({
             var instance = this;
 
             instance._unplugNodeFocusManager();
-
-            instance.fire('drag:keyDown', {ev: event});
-            event.halt();
         },
 
         /**
@@ -150,40 +89,11 @@ var Delegate = A.Component.create({
         _onCtrlKeyup: function(event) {
             var instance = this;
 
-            instance.fire('drag:mouseup');
+            instance.once('drag:keyDown', instance._onCtrlKeydown, instance);
 
-            if (DDM.activeDrag) {
-                DDM._end();
-            }
+            instance._plugNodeFocusManager();
 
-            if (instance._handle) {
-                instance._handle.detach();
-
-                event.target.focus();
-            }
-
-            instance._bindKeypressEvent();
-        },
-
-        /**
-        * TODO. Wanna help? Please send a Pull Request.
-        *
-        * @method _onDragStart
-        * @protected
-        */
-        _onDragStart: function(event) {
-            var instance = this,
-                drag = event.target,
-                doc = A.one(DOC),
-                dragNode = drag.get('node');
-
-            instance._targets = instance._getDropTargets();
-
-            instance._currentTarget = instance._targets.indexOf(dragNode);
-
-            instance._currentTarget = instance._currentTarget ? instance._currentTarget - 1 : instance._currentTarget;
-
-            event.halt();
+            event.target.blur().focus();
         },
 
         /**
@@ -196,10 +106,12 @@ var Delegate = A.Component.create({
             var instance = this,
                 container = A.one(instance.get(CONT));
 
-            container.plug(A.Plugin.NodeFocusManager, {
-                descendants: instance.get('nodes'),
-                keys: { next: 'down:39', previous: 'down:37' }
-            });
+            container.plug(
+                A.Plugin.NodeFocusManager, {
+                    descendants: instance.get('nodes'),
+                    keys: { next: 'down:39', previous: 'down:37' }
+                }
+            );
         },
 
         /**
